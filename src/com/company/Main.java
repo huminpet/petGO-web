dc110044d166064e48cc99bbc392b7e7f74cc0d3
胡敏 <hzhumin1@corp.netease.com>
2016-07-20 21:25:30 +0800

    hzhumin commit 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //test  aaaa   bbb  ddd   eeee
+        //test  aaaa   bbb  ddd  eee fff
         System.out.print("abc");
     }
 }
