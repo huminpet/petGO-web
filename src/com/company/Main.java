52896b678dcc009036d28ca36228cc65b2901347
humin <751478610@qq.com>
2016-07-20 06:30:00 -0700

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //test  aaaa   bbb  ddd  eee fff
+        //test  aaaa   bbb ccc
         System.out.print("abc");
     }
 }
