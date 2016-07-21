23e7540ab00d65e2b66715955a3090c8faa256a0
humin <751478610@qq.com>
2016-07-20 18:43:02 -0700

    crud

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //begin
+        //beginning
         System.out.print("abc");
-        //add update delete select
+        //add update delete select crud
     }
 }
