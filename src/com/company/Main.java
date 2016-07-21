37ab9a0e4899ff141b0e307c50ec1a3618d8b5c3
humin <751478610@qq.com>
2016-07-20 18:55:22 -0700

    delete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //begin and end
         System.out.print("abc");
-        //add update delete select crud ddm
+        //add update delete select crud 
     }
 }
