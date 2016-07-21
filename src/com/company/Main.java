30c09618e0e35ff571eaee185c2000a3291ecff5
humin <751478610@qq.com>
2016-07-20 18:45:56 -0700

    end

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //begin end
+        //begin and end
         System.out.print("abc");
         //add update delete select crud
     }
