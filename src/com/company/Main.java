c5b6436e30d0b10e97a7fdd7f7712593c959657e
胡敏 <hzhumin1@corp.netease.com>
2016-07-21 09:31:51 +0800

    hzhumin commit 8

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //abc
+        //abc   eeee
         System.out.print("abc");
         //add test
     }
